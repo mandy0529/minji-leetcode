f5c971e963ee56e9995009ea5af68e05a5519cf0
Minji Kim ✨ <59499779+mandy0529@users.noreply.github.com>
2023-08-24 15:43:26 +0900

    Time: 60 ms (50.14%), Space: 42 MB (73.46%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let maxProfit= 0;
+    for(let i = 0; i<prices.length; i++){
+        if(prices[i+1]>prices[i]) {
+            maxProfit= maxProfit+prices[i+1]-prices[i]
+        }
+    }
+    return maxProfit
+};
